93b33d819eed3445e89ee90961b3a1c48a98ac81
Billy-Night <billy.m.nightingale@gmail.com>
2022-03-20 13:58:19 +0100

    added abutton and selected it using DOM, created a function that produce a random number, then added a eventlistener that has a function that changes the background of the page when the button is clicked

--- index.js
+++ index.js
@@ -15,5 +15,26 @@ function changeColor () {
 function changeText () {
     clickText.textContent = "This text has been changed"
 };
+//Set up a variable for the button using querySelector
+const btn = document.querySelector('button');
+
+//Now set up a seperate functiion to forfill a task
+function random(number) {
+//The function will return a random number
+    return Math.floor(Math.random()*(number+1));
+}
+
+//Now set up an event listener which will look for a click event on the button
+btn.addEventListener('click', function() {
+    //Above will pass in the function of random(number) on every click event
+    const rndCol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+    //This will then set the backgroundcolor of the body to the above variable
+    document.body.style.backgroundColor = rndCol;
+});
+//The recommended mechanism for adding event handlers in web pages is the addEventListener() method
+//The function could also be changed to be outside of the addEventListener just like the function random it just needs to be defined with a name and then called:
+//syntax: btn.addEventListener('click', function name);
+
+
 
 
